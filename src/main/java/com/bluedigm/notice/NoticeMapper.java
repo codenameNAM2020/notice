7e1cb27abf0f4c02e7a19ddab4ab9f68b61209b1
codenameNAM2020 <codenam2020@gmail.com>
2022-05-04 18:34:15 +0900

    feat : getNoticeList 선언

--- src/main/java/com/bluedigm/notice/NoticeMapper.java
+++ src/main/java/com/bluedigm/notice/NoticeMapper.java
@@ -5,9 +5,9 @@ import org.apache.ibatis.annotations.Mapper;
 import java.util.List;
 
 @Mapper
-public interface NoticeMapper {
+public interface NoticeMapper {     //DAO
 
-    List<NoticeDto> getNoticeList(NoticeDto noticeDto);
+    List<NoticeDto> getNoticeList(NoticeDto noticeDao);
     NoticeDto getNotice(Integer id);
     Integer createNotice(NoticeDto noticeDao);
     Integer updateNotice(NoticeDto noticeDao);
